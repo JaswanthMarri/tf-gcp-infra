d34dc8deb3ecfad3eba0e454a45121ac47036b86
Jaswanth Marri <jeswanthjashu8@gmail.com>
2024-02-15 19:05:26 +0530

    Jaswanth feature (#3)
    * Update .gitignore
    
    * Create network.tf
    
    * Update network.tf
    
    * updated file
    
    updated file
    
    * Update network.tf
    
    * cred var updated
    
    ---------
    
    Co-authored-by: Jaswanth Marri <145034631+Jaswanth-marri@users.noreply.github.com>

--- network.tf
+++ network.tf
@@ -1,7 +1,21 @@
 provider "google" {
-  credentials = file("cloud-nw-dev-creds.json")
+  credentials = file(var.creds)
   project     = var.project_id
   region      = var.region
+}
+
+variable "creds" {}
+
+variable "vpc_name" {
+  default = "my-vpc"
+}
+
+variable "vpc_instance_name" {
+  default = "example-instance"
+}
+
+variable "zone" {
+  default = "us-east1-c"
 }
 
 variable "webapp_subnet_cidr" {
@@ -14,33 +28,40 @@ variable "db_subnet_cidr" {
 
 variable "project_id" {}
 variable "region" {}
+variable "webapp_subnet" {}
+variable "db_subnet" {}
+variable "route_name" {}
 
 resource "google_compute_network" "my_vpc" {
-  name                  = "my-vpc"
-  auto_create_subnetworks = false
-  routing_mode          = "REGIONAL"
+  name                            = var.vpc_name
+  auto_create_subnetworks         = false
+  routing_mode                    = "REGIONAL"
+  delete_default_routes_on_create = true
 }
 
-
 resource "google_compute_subnetwork" "webapp_subnet" {
-  name          = "webapp-subnet"
+  name          = var.webapp_subnet
+
   region        = var.region
   network       = google_compute_network.my_vpc.self_link
   ip_cidr_range = var.webapp_subnet_cidr
 }
 
 resource "google_compute_subnetwork" "db_subnet" {
-  name          = "db-subnet"
+  name          = var.db_subnet
+
   region        = var.region
   network       = google_compute_network.my_vpc.self_link
   ip_cidr_range = var.db_subnet_cidr
 }
 
+
 # Route for webapp subnet to access internet
 resource "google_compute_route" "webapp_route" {
-  name             = "webapp-to-internet"
+  name             = var.route_name
   network          = google_compute_network.my_vpc.self_link
   dest_range       = "0.0.0.0/0"
-  next_hop_ip      = google_compute_network.my_vpc.gateway_ipv4
+  next_hop_gateway = "global/gateways/default-internet-gateway"
+
   priority         = 1000
 }
