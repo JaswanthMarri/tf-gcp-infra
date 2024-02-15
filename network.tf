8a1092b288a857091b6124cc2df77b0f99f1ab9f
Jaswanth Marri <145034631+Jaswanth-marri@users.noreply.github.com>
2024-02-15 08:15:09 -0500

    updated file
    updated file

--- network.tf
+++ network.tf
@@ -4,6 +4,18 @@ provider "google" {
   region      = var.region
 }
 
+variable "vpc_name" {
+  default = "my-vpc"
+}
+
+variable "vpc_instance_name" {
+  default = "example-instance"
+}
+
+variable "zone" {
+  default = "us-east1-c"
+}
+
 variable "webapp_subnet_cidr" {
   description = "CIDR range for the webapp subnet"
 }
@@ -14,33 +26,37 @@ variable "db_subnet_cidr" {
 
 variable "project_id" {}
 variable "region" {}
+variable "webapp_subnet" {}
+variable "db_subnet" {}
+variable "webapp-to-internet" {}
 
 resource "google_compute_network" "my_vpc" {
   name                  = var.vpc_name
   auto_create_subnetworks = false
   routing_mode          = "REGIONAL"
+  delete_default_routes_on_create = true
 }
 
-
 resource "google_compute_subnetwork" "webapp_subnet" {
-  name          = "webapp"
+  name          = var.webapp_subnet
   region        = var.region
   network       = google_compute_network.my_vpc.self_link
   ip_cidr_range = var.webapp_subnet_cidr
 }
 
 resource "google_compute_subnetwork" "db_subnet" {
-  name          = "db"
+  name          = var.db_subnet
   region        = var.region
   network       = google_compute_network.my_vpc.self_link
   ip_cidr_range = var.db_subnet_cidr
 }
 
+
 # Route for webapp subnet to access internet
 resource "google_compute_route" "webapp_route" {
-  name             = "webapp-to-internet"
+  name             = var.route_name
   network          = google_compute_network.my_vpc.self_link
   dest_range       = "0.0.0.0/0"
-  next_hop_ip      = google_compute_network.my_vpc.gateway_ipv4
+  next_hop_gateway = "global/gateways/default-internet-gateway"
   priority         = 1000
 }
