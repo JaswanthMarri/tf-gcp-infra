ede0de643dcebe6d7758c8e3d1b64fd9c3b1cb5b
Jaswanth Marri <145034631+Jaswanth-marri@users.noreply.github.com>
2024-02-21 13:03:27 -0500

    variable updated and instance and firewall added
    variable updated and instance and firewall added

--- network.tf
+++ network.tf
@@ -33,10 +33,23 @@ variable "webapp_subnet" {}
 variable "db_subnet" {}
 variable "route_name" {}
 
+variable "ports" {}
+variable "routing_mode" {}
+variable "firewall_rule_name" {}
+variable "custom_image" {}
+variable "stack_type" {}
+
+variable "boot_disk_type" {}
+variable "boot_disk_size" {}
+variable "machine_type" {}
+variable "nw_tier" {}
+variable "prov_model" {}
+
+
 resource "google_compute_network" "my_vpc" {
   name                            = var.vpc_name
   auto_create_subnetworks         = false
-  routing_mode                    = "REGIONAL"
+  routing_mode                    = var.routing_mode
   delete_default_routes_on_create = true
 }
 
@@ -62,7 +75,81 @@ resource "google_compute_route" "webapp_route" {
   name             = var.route_name
   network          = google_compute_network.my_vpc.self_link
   dest_range       = "0.0.0.0/0"
-  next_hop_gateway = "global/gateways/default-internet-gateway"
-
+  next_hop_instance = google_compute_instance.vpc-instance-cloud.self_link
   priority         = 1000
+}
+
+#Firewall rule to allow traffic to the application port and deny SSH port from the internet
+resource "google_compute_firewall" "app_firewall" {
+  name    = var.firewall_rule_name
+  network = google_compute_network.my_vpc.self_link
+
+  allow {
+    protocol = "tcp"
+    ports    = [var.ports]  # Assuming app_port is a variable defining the application port
+  }
+  
+  allow {
+    protocol = "icmp"
+  }
+
+  source_ranges = ["0.0.0.0/0"]  # Allow traffic from the internet
+}
+
+
+resource "google_compute_instance" "vpc-instance-cloud" {
+  boot_disk {
+    auto_delete = true
+    device_name = var.vpc_instance_name
+
+    initialize_params {
+      image = var.custom_image
+      size  = var.boot_disk_size
+      type  = var.boot_disk_type
+    }
+
+    mode = "READ_WRITE"
+  }
+
+  can_ip_forward      = true
+  deletion_protection = false
+  enable_display      = false
+
+  labels = {
+    goog-ec-src = "vm_add-tf"
+  }
+
+  machine_type = var.machine_type
+  name         = var.vpc_instance_name
+
+  network_interface {
+    access_config {
+      network_tier = var.nw_tier
+    }
+
+    queue_count = 0
+    stack_type  = var.stack_type
+    subnetwork  = google_compute_subnetwork.webapp_subnet.self_link
+  }
+
+  scheduling {
+    automatic_restart   = true
+    on_host_maintenance = "MIGRATE"
+    preemptible         = false
+    provisioning_model  = var.prov_model
+  }
+
+ # service_account {
+  #  email  = "1027887585503-compute@developer.gserviceaccount.com"
+  #  scopes = ["https://www.googleapis.com/auth/devstorage.read_only", "https://www.googleapis.com/auth/logging.write", "https://www.googleapis.com/auth/monitoring.write", "https://www.googleapis.com/auth/service.management.readonly", "https://www.googleapis.com/auth/servicecontrol", "https://www.googleapis.com/auth/trace.append"]
+  #}
+
+  shielded_instance_config {
+    enable_integrity_monitoring = true
+    enable_secure_boot          = false
+    enable_vtpm                 = true
+  }
+
+  tags = ["http-server", "https-server"]
+  zone = var.zone
 }
